331fc5f487bb7594e8bf6116f1427c294c78c111
SHStorm <digitalsho19@gmail.com>
2019-07-06 23:00:18 +0500

    Solution

--- src/index.js
+++ src/index.js
@@ -3,5 +3,26 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let counter = 0;
+
+    const checkedIndexes = new Set();
+    for (let firstPersonIndex = 0; firstPersonIndex < preferences.length; firstPersonIndex++) {
+        if (checkedIndexes.has(firstPersonIndex)) continue;
+        
+        let secondPersonIndex = preferences[firstPersonIndex] - 1;
+        let thirdPersonIndex = preferences[secondPersonIndex] - 1;
+        let fourthPersonIndex = preferences[thirdPersonIndex] - 1;
+
+        if (thirdPersonIndex !== firstPersonIndex && fourthPersonIndex === firstPersonIndex) {
+            counter++;
+
+            checkedIndexes.add(firstPersonIndex);
+            checkedIndexes.add(secondPersonIndex);
+            checkedIndexes.add(thirdPersonIndex);
+        }
+
+        if (checkedIndexes.size === preferences.length) break;
+    }
+
+    return counter;
 };
